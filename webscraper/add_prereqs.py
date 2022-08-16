72d0eaca90730d5e602151d59b4129178eff30be
Zachary Hankin <zacharyjameshankin@gmail.com>
2022-08-16 22:53:31 +1200

    added the prereqs to DB

---
+++ webscraper/add_prereqs.py
@@ -0,0 +1,85 @@
+import json
+import sqlite3
+import re
+
+f = open('course_name_and_IDs.json')
+data = json.load(f)
+course_names = data[0]
+courseID = data[1]
+f = open('course_information.json')
+course_information = json.load(f)
+
+names = list(course_information.keys())
+
+all_courses_list = []
+for name in names:
+    for x in course_information[name]:
+        all_courses_list.append(x)
+        
+final_list_of_rest = []       
+for course in all_courses_list:
+    for req in course[3]:
+        if req.split(" ")[0] == "Prerequisite:":
+            restrictions = []
+            clean_req =  " ".join(req.split(" ")[1:])
+            clean_req=re.sub(",","",clean_req)
+            clean_req=clean_req.replace(".","")
+            
+            mainSubject = course[0]
+            mainID = course[1]
+            currentSubject = "P"
+            currentID = "P"
+            word_num = 0
+
+            toadd = []
+            temp_add = ''
+            
+            while word_num < len(clean_req.split(" ")[1:]):
+                temp_add = clean_req.split(" ")
+                x =clean_req.split(" ")[word_num]
+                if x not in course_names and x not in courseID:
+                    word_num = 10000000000000
+                    temp_add = ''
+                word_num += 1
+
+            main_subj = course[0]
+            main_id = course[1]
+
+            if temp_add != '':
+                rest_subj = "bad"
+                rest_id = "bad"
+                for x in temp_add:
+                    if x in course_names:
+                        rest_subj = x
+                    if x in courseID:
+                        rest_id = x
+                        final_list_of_rest.append((main_subj.split(" ")[0], main_subj.split(" ")[1], rest_subj, x))
+
+database = r"C:\Users\Zachary\Documents\GitHub\399capstone-p-np\399courses.db"
+
+
+sqliteConnection = sqlite3.connect(database)
+
+cursor = sqliteConnection.cursor()
+print("Successfully Connected to SQLite")
+
+
+sqlite_insert_query = """INSERT INTO preReq
+                      (preReqSubject, preReqNumber, subject, courseNumber) 
+                       VALUES 
+                      (?,?,?,?);"""
+
+for x in final_list_of_rest:
+    try:
+        cursor.execute(sqlite_insert_query, x)
+    except sqlite3.IntegrityError as er:
+        print("Don't worry:", er.args[0], x)
+sqliteConnection.commit()
+print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+cursor.close()
+
+        
+
+
+
+
