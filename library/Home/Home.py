a23eb22aa7dfed76e7678d08f9d3ea22cb4d8a4a
Ch-r-i-s <88464424+Ch-r-i-s@users.noreply.github.com>
2022-08-31 16:46:17 +1200

    Main GUI adaptation started

--- library/Home/Home.py
+++ library/Home/Home.py
@@ -15,7 +15,7 @@ home_blueprint = Blueprint(
 
 @home_blueprint.route('/', methods=['GET'])
 def home():
-    semesters = [["a", "b"],[""],[""]]
+    semesters = [["Semester 1",["399", "3 courses at 3rd level"], ["315 Compsci", "215 Compsci"]],["Semester 2",["215 Compsci", "120 Compsci"]],["Semester 1",["230 Compsci", "130 Compsci"]]]
     return render_template(
         'Home_Page.html',
         semesters=semesters,
