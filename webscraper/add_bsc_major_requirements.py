203178929da2616517a67d2d2178b6bf549a2e61
Alex Baldwin <alex.andersonbaldwin@gmail.com>
2022-09-12 21:07:47 +1200

    Scrapes about 80% of course requirement data correctly but there are still quite a few courses with problematic formats

--- webscraper/add_bsc_major_requirements.py
+++ webscraper/add_bsc_major_requirements.py
@@ -5,6 +5,7 @@ import numpy as np
 from unicodedata import normalize
 from bs4 import BeautifulSoup
 import json
+import re
 
 
 def getMajorLinks(url):
@@ -29,7 +30,8 @@ def getMajorLinks(url):
     
     return links
 
-def getStudyLinks(url):
+
+def getSectionLinks(url):
     x = 0
     while x < 10:
         try:
@@ -49,13 +51,63 @@ def getStudyLinks(url):
 
     return links
 
+def getMajorReq(url):
+    x = 0
+    while x < 10:
+        try:
+            response = urlrq.urlopen(url, context=ssl.create_default_context(cafile=certifi.where()))
+            x = 100
+        except:
+            x += 1
+    if x != 100:
+        print("read fail on ", url)
+        return []
+    html_doc = response.read()
+    soup = BeautifulSoup(html_doc, 'html.parser')
     
+    reqs = []
+    courses = []
+    major = []
+    print(soup.title.string)
+    #use url to get name of course, honours, and before/from 2019
+    major.append(re.split("/", url[70:]))
+    #major.append(re.findall("\(B[A-z]+.*\s", soup.title.string))
+    #major.append(re.findall("in .+,", soup.title.string)[1:])
+    for text in soup.strings:
+       # print(text + "*****")
+        if re.search("[0-9]+\spoints.*", text):
+            if len(courses) > 1:
+                reqs.append(courses)
+            courses = [re.findall("[0-9]+", text)]
+            if len(courses) != 1:
+                courses = []
+        if re.search("[a-z]+.*[A-Z][A-Z][A-Z]*\s[0-9][0-9][0-9].*", text):
+            courses.append("Manual entry required")
+            courses.append(text)
+        elif re.search("[A-Z][A-Z][A-Z]*\s[0-9][0-9][0-9].*", text) != None:
+            courses.append(text.split()[:2])
+        
+    return (major,reqs[1:])
 
+
+
+majors = []
 url = "https://www.auckland.ac.nz/en/study/study-options/find-a-study-option/bachelor-of-science-bsc.html"
 links = getMajorLinks(url)
 for link in links:
-    for study_link in getStudyLinks(link):
-        print(study_link)
+    for section_link in getSectionLinks(link):
+        for section_link_2 in getSectionLinks(section_link):
+            print(section_link_2)
+            major = getMajorReq(section_link_2)
+            majors.append(major)
+            print(major)
+
+json_data = json.dumps(majors, indent = 4)
+with open("major_reqs.json", "w") as outfile:
+    outfile.write(json_data)
+
+            
+            
 
 
 
