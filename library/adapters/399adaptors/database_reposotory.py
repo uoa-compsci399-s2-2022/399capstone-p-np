50391fe6b3890ffe02bcf8b9f4cc50518cd51dbc
Zachary Hankin <zacharyjameshankin@gmail.com>
2022-09-16 12:19:23 +1200

    set up data

--- library/adapters/399adaptors/database_reposotory.py
+++ library/adapters/399adaptors/database_reposotory.py
@@ -1,151 +1,42 @@
-from datetime import date
-from typing import List
+import json
+import sqlite3
+import re
 
-from sqlalchemy import desc, asc
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
-from sqlalchemy.orm import scoped_session
-from flask import _app_ctx_stack
+#use OS to change path dynamically
+database = r"C:\Users\Zachary\Documents\GitHub\399capstone-p-np\399courses.db"
 
-from library.domain.model import User, Book, Author, Publisher
-from library.adapters.repository import AbstractRepository
 
-#testing push to main
-class SessionContextManager:
-    def __init__(self, session_factory):
-        self.__session_factory = session_factory
-        self.__session = scoped_session(self.__session_factory, scopefunc=_app_ctx_stack.__ident_func__)
+#Connects to database file
+sqliteConnection = sqlite3.connect(database)
+cursor = sqliteConnection.cursor()
+print("Successfully Connected to SQLite")
 
-    def __enter__(self):
-        return self
 
-    def __exit__(self, *args):
-        self.rollback()
+#The SQL query, the ?s are the data
+sqlite_insert_query = """select pointsrequired majorname from majors;"""
 
-    @property
-    def session(self):
-        return self.__session
 
-    def commit(self):
-        self.__session.commit()
+#calls the SQL command here. If key duplication error then its fine. Anything else is an issue
 
-    def rollback(self):
-        self.__session.rollback()
+c = cursor.execute(sqlite_insert_query)
 
-    def reset_session(self):
-        # this method can be used e.g. to allow Flask to start a new session for each http request,
-        # via the 'before_request' callback
-        self.close_current_session()
-        self.__session = scoped_session(self.__session_factory, scopefunc=_app_ctx_stack.__ident_func__)
 
-    def close_current_session(self):
-        if not self.__session is None:
-            self.__session.close()
+rows = result.fetchall();
 
-class SqlAlchemyRepository():
+#Commits and saves.
+sqliteConnection.commit()
+print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+cursor.close()
 
-    def __init__(self, session_factory):
-        self._session_cm = SessionContextManager(session_factory)
 
-    def close_session(self):
-        self._session_cm.close_current_session()
 
-    def reset_session(self):
-        self._session_cm.reset_session()
-
-    def add_user(self, user: User):
-        with self._session_cm as scm:
-            scm.session.add(user)
-            scm.commit()
-
-    def get_user(self, user_name: str):
-        user = None
-        try:
-            user = self._session_cm.session.query(User).filter(User._User__user_name == user_name).one()
-        except NoResultFound:
-            return None
-        return user
-
-    def add_book(self, book):
-        with self._session_cm as scm:
-            scm.session.add(book)
-            scm.commit()
-
-    def get_book_by_title(self, title: str):
-        book = None
-        try:
-            book = self._session_cm.session.query(Book).filter(Book._Book__title == title).one()
-        except NoResultFound:
-            return None
-        return book
-
-    def get_book_by_year(self, year: str):
-        book = None
-        try:
-            book = self._session_cm.session.query(Book).filter(Book._Book__release_year == year).all()
-        except NoResultFound:
-            return None
-        return book
-
-    def get_book_by_author(self, year: str):
-        book = None
-        try:
-            book = self._session_cm.session.query(Book).filter().all()
-        except NoResultFound:
-            return None
-        return book
-
-    def get_book_by_publisher(self, name: str):
-        book = None
-        try:
-            book = self._session_cm.session.query(Book).filter(Book._Book__publisher_id == name).all()
-        except NoResultFound:
-            return None
-        return book
-
-    def get_book_by_id(self, name: str):
-        book = None
-        try:
-            book = self._session_cm.session.query(Book).filter(Book._Book__book_id == name).one()
-        except NoResultFound:
-            return None
-        return book
-
-    def add_publisher(self, publisher):
-        with self._session_cm as scm:
-            scm.session.add(publisher)
-            scm.commit()
-
-    def get_publisher(self, name: str):
-        publisher = None
-        try:
-            publisher = self._session_cm.session.query(Publisher).filter(Publisher._Publisher__name == name).one()
-        except NoResultFound:
-            return None
-        return publisher
-
-    def add_author(self, author):
-        with self._session_cm as scm:
-            scm.session.add(author)
-            scm.commit()
-
-    def get_author(self, name: str):
-        author = None
-        try:
-            author = self._session_cm.session.query(Author).filter(Author._Author__full_name == name).one()
-
-        except NoResultFound:
-            return None
-        return author
-
-    def get_all_books(self):
-        try:
-            publisher = self._session_cm.session.query(Book).filter().all()
-        except NoResultFound:
-            return None
-        return publisher
-
-    def add_review(self, review):
-        with self._session_cm as scm:
-            scm.session.add(review)
-            scm.commit()
+#list of all possible courses that exit
+#a function of all courses they need to take to graduate required courses,(group courses, points))
+#given name of courses they are taking and what time, return same matrix with null, or error message
+#given list of courses if they will graduate
+#Return all courses that can be taken at that time
+#return a list of courses they can take given what they are doing
+#Returns unclassified errors with a specific course
+#given a course return course description
+#given a coure return all course information
