fa23ad0b0a5c878ac323ab81e62ad87e9310e81f
Zachary Hankin <zacharyjameshankin@gmail.com>
2022-09-18 20:02:37 +1200

    more funks

--- library/adapters/399DbFunctions.py
+++ library/adapters/399DbFunctions.py
@@ -21,12 +21,21 @@ def return_isolated_problems_with_course(course_subject, course_number):
     else:
         return "does not exist"
 
-def return_course_desc(course_subject, course_number):
+def return_course_description(course_subject, course_number):
     a = cursor.execute("select * from course where subject = ? and courseNumber = ?", (course_subject, course_number))
     list_of_courses = a.fetchall()
     if len(list_of_courses) == 1:
         course = list_of_courses[0]
         return course[7]
+    else:
+        return "does not exist"
+
+def return_all_course_information(course_subject, course_number):
+    a = cursor.execute("select * from course where subject = ? and courseNumber = ?", (course_subject, course_number))
+    list_of_courses = a.fetchall()
+    if len(list_of_courses) == 1:
+        course = list_of_courses[0]
+        return course
     else:
         return "does not exist"
 
@@ -40,3 +49,13 @@ def return_course_points(course_subject, course_number):
 
 def problems_with_timetable(timetable):
     #the timetable is given as a list of all subjects done in (y1S1, y1S2, y2s1, y2s2) 
+    for x in timetable:
+        pass
+    
+
+#a function of all courses they need to take to graduate required courses,(group courses, points))
+#given name of courses they are taking and what time, return same matrix with null, or error message
+#given list of courses if they will graduate
+#Return all courses that can be taken at that time
+#return a list of courses they can take given what they are doing
+#given a coure return all course information
