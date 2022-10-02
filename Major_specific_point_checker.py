f5d161d4799b1f4682cc08ed9ae50c5205fec1fa
dionneavis <110002533+dionneavis@users.noreply.github.com>
2022-10-03 11:46:22 +1300

    Update Major_specific_point_checker.py

--- Major_specific_point_checker.py
+++ Major_specific_point_checker.py
@@ -4,13 +4,13 @@ def major_specific_points_check(Subject,Subject_number,Major):
     a = self.__cursor.execute("""select 300_LEVEL_POINTS_MAJOR_SPECIFIC from majorRequirements WHERE majorNAME=?""", (Major))
     points = a.fethall()
     paper = points / 15  # assuming paper worth 15 points
-    paper_count = 0
+    point_count = 0
 
     for i in len(Subject):
         if(Subject[i]==Major):
             if(Subject_number[i]>299 AND(Subject_number[i]<399 ):
-                paper_count = paper_count+1
-    if(paper_count<paper):
+                point_count = point_count+return_course_points(Subject,Subject_number)
+    if(point_count<paper):
         print("Not enough 300 level papers within the users major")
         return 0
 
