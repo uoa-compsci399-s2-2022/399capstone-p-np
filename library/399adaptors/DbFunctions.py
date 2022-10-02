40ce1a94fe383a46c1d3655c05c768962c204f4e
dionneavis <110002533+dionneavis@users.noreply.github.com>
2022-10-03 11:47:50 +1300

    Update DbFunctions.py

--- library/399adaptors/DbFunctions.py
+++ library/399adaptors/DbFunctions.py
@@ -127,6 +127,24 @@ class searchTool:
             new_timetable.append(sem)
             probs.append([[course[0]+ " " + course[1], self.worst_problems_with_course(course[0],course[1], new_timetable),self.return_isolated_problems_with_course( course[0],  course[1]) ] for course in sem])
         return probs
+    
+
+## CHECKS the user is taking enough 300 level points within their specific major
+    def major_specific_points_check(Subject,Subject_number,Major):
+        a = self.__cursor.execute("""select 300_LEVEL_POINTS_MAJOR_SPECIFIC from majorRequirements WHERE majorNAME=?""", (Major))
+        points = a.fethall()
+        paper = points / 15  # assuming paper worth 15 points
+        point_count = 0
+
+        for i in len(Subject):
+            if(Subject[i]==Major):
+                if(Subject_number[i]>299 AND(Subject_number[i]<399 ):
+                    point_count = point_count+return_course_points(Subject,Subject_number)
+        if(point_count<paper):
+            print("Not enough 300 level papers within the users major")
+            return 0
+
+        return 1
 
     def will_graduate(self, timetable, majorname):
         done_courses = []
