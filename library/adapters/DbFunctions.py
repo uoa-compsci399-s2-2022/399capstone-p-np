dfe34f7aa690e5fda3862ad829959d5647b6f2e9
Zachary Hankin <zacharyjameshankin@gmail.com>
2022-10-06 19:00:28 +1300

    simplified reletive path

--- library/adapters/DbFunctions.py
+++ library/adapters/DbFunctions.py
@@ -5,9 +5,7 @@ import time
 class searchTool:
     def __init__(self):
         #sqliteConnection = sqlite3.connect(r"library\adapters\399courses.db")
-        save_place = os.getcwd().split("\\").index("399capstone-p-np") + 1
-        print("\\".join(os.getcwd().split("\\")[:save_place]) + "\\library\\adapters\\399courses.db")
-        sqliteConnection = sqlite3.connect("\\".join(os.getcwd().split("\\")[:save_place]) + "\\library\\adapters\\399courses.db")
+        sqliteConnection = sqlite3.connect("\\".join(os.getcwd().split("\\")[:os.getcwd().split("\\").index("399capstone-p-np") + 1]) + "\\library\\adapters\\399courses.db")
 
         self.__cursor = sqliteConnection.cursor()
 
